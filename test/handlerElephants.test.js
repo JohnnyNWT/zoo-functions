ab086e98e2a270325e5b765a11ac2058ae294885
JohnnyNWT <jooaaooskt@gmail.com>
2022-07-29 17:28:41 -0300

    Req - 6 - Cobertura 80%

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,15 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se para o argumento "count" retorna o número inteiro "4"', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+
+  it('Verifica se para o argumento "names" retorna um array de nomes que possui o nome "Jefferson"', () => {
+    expect(handlerElephants('names')).toEqual([ 'Ilana', 'Orval', 'Bea', 'Jefferson' ]);
+  });
+
+  it('Verifica se para o argumento "averageAge" retorna um número próximo a "10.5"', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+});
