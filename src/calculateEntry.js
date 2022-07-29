1abd384379add4669d4dbf7622084669fa241e99
JohnnyNWT <jooaaooskt@gmail.com>
2022-07-29 13:54:46 -0300

    Erros Lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,30 +1,30 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  const objeto = {child: 0, adult: 0, senior: 0}
+  const objeto = { child: 0, adult: 0, senior: 0 };
   entrants.forEach((element) => {
     if (element.age < 18) {
-      objeto.child += 1
+      objeto.child += 1;
     } else if (element.age >= 18 && element.age < 50) {
-      objeto.adult += 1
+      objeto.adult += 1;
     } else {
-      objeto.senior += 1
+      objeto.senior += 1;
     }
-  })
-  return objeto
+  });
+  return objeto;
 }
 
 function calculateEntry(entrants) {
-  if (!entrants || Object.values(entrants).length === 0) { 
-    return 0; 
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
   }
   let inputValue;
   const { prices } = data;
   const arrPrices = [prices];
   const participantes = countEntrants(entrants);
-  arrPrices.forEach((element) => inputValue = element.adult * participantes.adult + 
-  element.senior * participantes.senior + 
-  element.child * participantes.child);
+  arrPrices.forEach((element) => {
+  inputValue = element.adult * participantes.adult + element.senior * participantes.senior + element.child * participantes.child;
+});
   return inputValue;
 }
 
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,8 +2,8 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (typeof employeeName !== 'undefined') {
-    return data.employees.find((element) => 
-    element.firstName === employeeName || element.lastName === employeeName);
+    return data.employees.find((element) =>
+      element.firstName === employeeName || element.lastName === employeeName);
   }
   return {};
 }
