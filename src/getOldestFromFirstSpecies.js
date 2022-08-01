bd36840241b67cdd6a39aad1303c9eee894eeb5a
JohnnyNWT <jooaaooskt@gmail.com>
2022-08-01 16:25:30 -0300

    Req - 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const person = data.employees.find((element) => element.id === id).responsibleFor;
+  const firstSpecie = data.species.find((element) => element.id === person[0]).residents;
+  const order = firstSpecie.sort((firstElement, secondElement) => secondElement.age - firstElement.age)
+  return Object.values(order[0])
 }
 
 module.exports = getOldestFromFirstSpecies;
