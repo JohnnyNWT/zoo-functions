1931239d4c12a9d9e45d31ea85738f4b15e1d103
JohnnyNWT <jooaaooskt@gmail.com>
2022-08-01 16:53:03 -0300

    Lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   const person = data.employees.find((element) => element.id === id).responsibleFor;
   const firstSpecie = data.species.find((element) => element.id === person[0]).residents;
-  const order = firstSpecie.sort((firstElement, secondElement) => secondElement.age 
+  const order = firstSpecie.sort((firstElement, secondElement) => secondElement.age
   - firstElement.age);
   return Object.values(order[0]);
 }
