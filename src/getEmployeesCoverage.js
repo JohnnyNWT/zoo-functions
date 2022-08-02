d5e885c06ff1634f511bae902eaa9067a2ae492a
JohnnyNWT <jooaaooskt@gmail.com>
2022-08-02 18:57:37 -0300

    Req - 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,9 @@
 const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getElement = (obj) => employees.find((element) => element.firstName === obj.name || element.lastName === obj.name || element.id === obj.id);
+const getElement = (obj) => employees.find((element) => element.firstName === obj.name
+|| element.lastName === obj.name
+|| element.id === obj.id);
 const searchAnimal = (animal) => animal.responsibleFor.map((id) => species.find((element) => (element.id === id)));
 const listAllPerson = () => employees.map((elemento) => ({
   id: elemento.id,
